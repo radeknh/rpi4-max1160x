7cefd0cab6238544f3f32e585fdae890a178c95a
radeknh <radeknh@users.noreply.github.com>
2021-10-24 18:59:10 +0200

    Update max1160x-read.cpp

--- max1160x-read.cpp
+++ max1160x-read.cpp
@@ -1,10 +1,6 @@
 /******************************************************************************
-i2ctest.cpp
+max1160x-read.cpp
 Raspberry Pi 4 - test of MAX11602 read
-
-This example makes use of the Wiring Pi library
-The I2C API is documented here:
-https://projects.drogon.net/raspberry-pi/wiringpi/i2c-library/
 ******************************************************************************/
 
 #include <iostream>
@@ -13,7 +9,40 @@ https://projects.drogon.net/raspberry-pi/wiringpi/i2c-library/
 
 using namespace std;
 
-#define MAX11602_ADR 0x6d
+#define MAX116xx_ADR    0x6d  // MAX11602 - 0x6d
+#define RESOLUTION      8     // number of bits
+
+void config(uint8_t data, int* fd)
+{
+   wiringPiI2CWrite(*fd, data & 0b01111111); // MSB=0 chooses a configuration byte (7 bits)
+}
+
+void setup(uint8_t data, int* fd)
+{
+   wiringPiI2CWrite(*fd, data | 0b10000000); // MSB=1 chooses a setup byte (7 bits)
+}
+
+uint16_t readSingleCh(uint8_t channel, int* fd)
+{
+   uint16_t result = 0x0000;
+
+   uint8_t configurationByte = ( (channel<<1) & 0b00011110) | 0b01100001; //single ended mode, convert a single channel
+   config(configurationByte);
+
+#if RESOLUTION > 8
+   // for more than 8-bit MAX116xx
+   result = (wiringPiI2CRead(*fd) & 0x03)<<8; // MSB is returned first. [7-2] are high.
+   result |= wiringPiI2CRead(*fd) & 0x00FF; // read LSB
+#elseif
+   result = wiringPiI2CRead(*fd) & 0xFFFF;   
+#endif
+   return result;
+}
+
+/*void readNChTest(int n, int &fd)
+{
+   
+}*/
 
 int main()
 {
@@ -21,23 +50,20 @@ int main()
 
    // Initialize the interface by giving it an external device ID instead of I2C device address.
    // It returns a standard file descriptor.
-   fd = wiringPiI2CSetup(MAX11602_ADR);
+   fd = wiringPiI2CSetup(MAX116xx_ADR);
    if(fd < 0) cout << "I2C initialization error. Errno is: " << errno << endl;
 
    // configuration byte - for single channel conversion, single-ended mode
-   wiringPiI2CWrite(fd, 0b01100001);
+   config(0b01100001, &fd);
    // setup byte - ref = VDD, internal clock, unipolar mode
-   wiringPiI2CWrite(fd, 0b10000010);
+   setup(0b10000010, &fd);
 
-   for(int i = 0; i < 1; i++)
+   result = readSingleCh(1, &fd);
+
+   if(result == -1)
    {
-      result = wiringPiI2CRead(fd);
-
-      if(result == -1)
-      {
-         cout << "Error.  Errno is: " << errno << endl;
-      }else{
-         cout << "Result: " << result << endl;
-      }
+      cout << "Error.  Errno is: " << errno << endl;
+   }else{
+      cout << "Result: " << result << endl;
    }
 }
